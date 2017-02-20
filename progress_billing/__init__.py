85dd4b112fa08c8ab8e3e33bec9299bb15f9fe91
Khalid Said <kerbrose@hotmail.com>
2017-02-20 11:22:43 +0200

    preparing module progress_billing as well as adding model construction master format with a parent child relations (Nested Set Model).

---
+++ progress_billing/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 All Rights Reserved
+#
+# Created on Feb 20, 2017
+#
+# @author: kerbrose (Khaled Said)
+# @contact: kerbrose  ==> hotmail  ==> com
+#####################################
+
+from . import models
---
+++ progress_billing/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 All Rights Reserved
+#
+# Created on Feb 20, 2017
+#
+# @author: kerbrose (Khaled Said)
+# @contact: kerbrose  ==> hotmail  ==> com
+#####################################
+
+{
+    'name': "Progress Billing",
+    'summary': "This is a module for progress billing invoicing",
+    'description': """This module can be used in construction industry""",
+    'author': "ϰhalid Said (Kerbrose)",
+    'license': "AGPL-3",
+    'website': "https://gitlab.com/kerbrose/odoo-arabia",
+    'category': 'Accounting',
+    'version': '10.0.0.1',
+    'depends': ['account_accountant', 'purchase'],
+    'data': [ ],
+    'demo': [ ],
+    'application': True,
+    'installable': True,
+}
